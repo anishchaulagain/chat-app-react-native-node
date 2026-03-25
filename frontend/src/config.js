e1245d6bcc3d4da9468c272e5555e27f316f5b0f
Anish Chaulagain <anishchaulagain2058@gmail.com>
2026-03-25 21:34:32 +0545

    swap port for frontend and backend

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
-const API_URL = 'http://localhost:3000'; // Using localhost for web/desktop. Use your IP for real device/emulator.
-const WS_URL = 'ws://localhost:3000';
+const API_URL = 'http://localhost:8000'; // Using localhost for web/desktop. Use your IP for real device/emulator.
+const WS_URL = 'ws://localhost:8000';
 
 export default {
   API_URL,
